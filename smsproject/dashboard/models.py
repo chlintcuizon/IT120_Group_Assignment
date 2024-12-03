3ac428ae41ffe95192ef235420831079c4db2e47
AngelLeoKim <torotorokuyakim1011@gmail.com>
2024-12-03 16:50:56 +0800

    feat: added modals for account and message function

--- smsproject/dashboard/models.py
+++ smsproject/dashboard/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 import os
 from twilio.rest import Client
@@ -10,4 +9,26 @@ class Message(models.Model):
     def __str__(self):
         return self.name
     
-   
+    def save(self, *args, **kwargs):
+        if self.score >=70:
+            account_sid = 'ACc826875119450c4c8a97a337f4ffa2ce'
+            auth_token = '30116837c1c5d2c36ffdc7059f622f4b'
+            client = Client(account_sid, auth_token)
+
+            message = client.messages.create(
+                    body=f"Congratulations! {self.name}, your score is {self.score}",
+                    from_="+16812460290",
+                    to="+639667506130",
+                    )
+        else:
+             account_sid = 'ACc826875119450c4c8a97a337f4ffa2ce'
+             auth_token = '30116837c1c5d2c36ffdc7059f622f4b'
+             client = Client(account_sid, auth_token)
+
+             message = client.messages.create(
+                    body=f"Sorry! {self.name}, your score is {self.score}",
+                    from_="+16812460290",
+                    to="+639667506130",
+                    )
+        print(message.sid)
+        return super().save(*args, **kwargs)
