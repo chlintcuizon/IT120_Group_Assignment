f610dcd648068064d872eac07ff9ce1946727f89
chlintcuizon <chlint.cuizon@carsu.edu.ph>
2024-12-03 17:51:00 +0800

    fix: change twilio account

--- smsproject/dashboard/models.py
+++ smsproject/dashboard/models.py
@@ -11,24 +11,24 @@ class Message(models.Model):
     
     def save(self, *args, **kwargs):
         if self.score >=70:
-            account_sid = 'ACc826875119450c4c8a97a337f4ffa2ce'
-            auth_token = '30116837c1c5d2c36ffdc7059f622f4b'
+            account_sid = 'AC1b19cb75cd7b50ca291b1d8bf468927b'
+            auth_token = '0f89eb4c0538f801955f243ddd16875c'
             client = Client(account_sid, auth_token)
 
             message = client.messages.create(
                     body=f"Congratulations! {self.name}, your score is {self.score}",
-                    from_="+16812460290",
-                    to="+639667506130",
+                    from_="+17753739447",
+                    to="+639171887729",
                     )
         else:
-             account_sid = 'ACc826875119450c4c8a97a337f4ffa2ce'
-             auth_token = '30116837c1c5d2c36ffdc7059f622f4b'
+             account_sid = 'AC1b19cb75cd7b50ca291b1d8bf468927b'
+             auth_token = '0f89eb4c0538f801955f243ddd16875c'
              client = Client(account_sid, auth_token)
 
              message = client.messages.create(
                     body=f"Sorry! {self.name}, your score is {self.score}",
-                    from_="+16812460290",
-                    to="+639667506130",
+                    from_="+17753739447",
+                    to="+639171887729",
                     )
         print(message.sid)
         return super().save(*args, **kwargs)
