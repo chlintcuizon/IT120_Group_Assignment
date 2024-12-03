beeab2af7448ce844107f36cca1a728fe28c8cb2
JTubo <joshuatubo4@gmail.com>
2024-12-03 15:38:55 +0800

    feat: added dashboard folder/ feat:added code for admin.py/ added code for models

--- smsproject/dashboard/models.py
+++ smsproject/dashboard/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
+from django.db import models
+import os
+from twilio.rest import Client
 # Create your models here.
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
+   
